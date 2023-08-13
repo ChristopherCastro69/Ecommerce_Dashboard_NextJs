03fc547e8319e1b7fbc1492b6c1c49a0d7d2535e
Topitops69 <topitops123@gmail.com>
2023-08-14 02:08:07 +0800

    completed the CRUD methods

--- src/pages/api/products.js
+++ src/pages/api/products.js
@@ -1,12 +1,25 @@
 
 import { mongooseConnect } from "@/lib/mongoose";
-import {Product} from "@/models/Product";
+import {Product} from "@/pages/api/models/Product";
 
 //Main product from Mongo DB
 export default async function handle(req, res){    
     // res.json(req.method);
     const {method} = req;
     await mongooseConnect();
+
+    //Read
+    if(method === 'GET'){
+        // if naay id
+        if(req.query?.id){
+            res.json(await Product.findOne({_id:req.query.id}));
+        } else{
+            res.json(await Product.find());
+        }
+     
+    }
+
+    //Create
     if(method === 'POST'){
         const {title, description, price} = req.body;
 
@@ -14,5 +27,20 @@ export default async function handle(req, res){
             title, description, price,
         })
         res.json(productDoc);
+    }
+
+    //Update
+    if(method === 'PUT'){
+        const {title, description, price, _id} = req.body;
+        await Product.updateOne({_id}, {title, description, price});
+        res.json(true);
+    }   
+
+    //Delete
+    if(method === 'DELETE'){
+        if(req.query?.id){
+            await Product.deleteOne({_id:req.query?.id});
+            res.json(true);
+        }
     }
 }
---
+++ src/pages/components/ProductForm.js
@@ -0,0 +1,87 @@
+import { useState } from "react";
+
+import axios from "axios";
+import { useRouter } from "next/router";
+
+export default function ProductForm({
+    _id,
+    title: existingTitle, 
+    description: existingDescription, 
+    price: existingPrice,
+    images,
+}){
+
+    const [title, setTitle] = useState(existingTitle || '');
+    const [description, setDescription] = useState(existingDescription || '');
+    const [price, setPrice] = useState(existingPrice || '');
+    const [goToProducts, setGoToProducts] = useState(false);
+    const router = useRouter();
+
+    // for database axios
+    async function saveProduct(ev){
+        ev.preventDefault();
+        // if there is an id for the product
+        const data = {title, description, price};
+        if(_id){
+            //update
+
+            await axios.put('/api/products', {...data,_id});
+
+        }else{
+            //create
+      
+            await axios.post('/api/products', data);
+          
+        }
+        setGoToProducts(true);
+        
+    }
+        if(goToProducts){
+            router.push('/products');
+        }
+    return (
+
+            <form onSubmit={saveProduct}>
+                
+                <label>Product name</label>
+                <input 
+                type="text" 
+                placeholder="product name"
+                value ={title}
+                onChange = {ev => setTitle(ev.target.value)}/>
+
+            <label>
+                Photos 
+            </label>
+            <div>
+                   {!images?.length && (
+                    <div>
+                        No Photos in this product
+                    </div>
+                )}
+            </div>
+
+
+                <label>Description</label>
+                <textarea 
+                placeholder="description"
+                value={description}
+                onChange={ev => setDescription(ev.target.value)}
+                />        
+
+
+                <label>Price</label>
+                <input 
+                type="number" 
+                placeholder="price"
+                value={price}
+                onChange={ev => setPrice(ev.target.value)}
+                />
+
+                <button type="Submit"
+                    className="btn-primary">Save
+                </button>
+            </form>
+       
+    )
+}
