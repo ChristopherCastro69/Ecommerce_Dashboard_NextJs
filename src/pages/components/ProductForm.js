bff70696fc7a59c9435216f7e0b4b733b01f1801
Topitops69 <topitops123@gmail.com>
2023-08-16 21:43:36 +0800

    env fix

--- src/pages/components/ProductForm.js
+++ src/pages/components/ProductForm.js
@@ -79,6 +79,10 @@ export default function ProductForm({
                 value ={title}
                 onChange = {ev => setTitle(ev.target.value)}/>
 
+            <label >Category</label>
+            <select >
+                <option value="">Uncategorized</option>
+                </select>
             <label>
                 Photos 
             </label>
@@ -108,13 +112,11 @@ export default function ProductForm({
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5m-13.5-9L12 3m0 0l4.5 4.5M12 3v13.5" />
                     </svg>
-
                     Upload
                     <input type="file" onChange={uploadImages} className="hidden"/>
                 </label>
                    
             </div>
-
 
                 <label>Description</label>
                 <textarea 
@@ -122,7 +124,6 @@ export default function ProductForm({
                 value={description}
                 onChange={ev => setDescription(ev.target.value)}
                 />        
-
 
                 <label>Price</label>
                 <input 
